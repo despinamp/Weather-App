8c405b113f30e5c762ac802d96306281cb5cb8b5
despinamp <despina.bourou@gmail.com>
2024-08-27 16:49:45 +0300

    unitgroup

--- weathercode.js
+++ weathercode.js
@@ -1,42 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
     let incelsius=true; //default degrees are celsius when first visiting app
-    let tempval,feelslikeval;
+    let tempval,feelslikeval,unitGroup;
     const monthArr=['January','February','March','April','May','June','July','August','September','October','November','December'];
-   
-    //farenheit to celsius
-    const ftoc=()=>{
-        if (incelsius===false){        
-            incelsius=true;           
-            const newtemp=(tempval-32)*5/9;
-            const newfeelslike=(feelslikeval-32)*5/9;
-            tempval=newtemp;
-            feelslikeval=newfeelslike;                            
-            document.getElementById('temp').innerHTML=newtemp+'°C';
-            document.getElementById('feelslike').innerHTML=newfeelslike+'°C';
-        }
-    }
-
-    //celsius to farenheit
-    const ctof=()=>{        
-        if (incelsius===true){
-            incelsius=false; 
-            const newtemp=(tempval*9/5)+32;
-            const newfeelslike=(feelslikeval*9/5)+32;
-            tempval=newtemp;
-            feelslikeval=newfeelslike;            
-            document.getElementById('temp').innerHTML=newtemp+'°F';
-            document.getElementById('feelslike').innerHTML=newfeelslike+'°F';         
-            
-        }
-
-    }   
-
     document.getElementById('farenheit').onclick=ctof;
     document.getElementById('celsius').onclick=ftoc; 
 
     //fetching data from visual crossing api
     const getweatherinfo=(locationvalue)=>{
-        url='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+locationvalue+'?key=FZRM4QGNURRJWUZU2H8CZYKX2'
+        if (incelsius===true){
+            unitGroup='metric'; //unitGroup parameter available in url, metric returns temp in celsius, us in farenheit
+        }
+        else{
+            tempsymbol='°F';
+            unitGroup='us';
+        }
+        url='https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'+locationvalue+'/?'+unitGroup+'&key=FZRM4QGNURRJWUZU2H8CZYKX2'
         
         fetch(url)
         .then(response=>{
@@ -52,14 +30,8 @@ document.addEventListener('DOMContentLoaded',function(){
             const monthString=monthArr[parseInt(dateArr[1])-1];
             const dayInt=parseInt(dateArr[2]);
             const fullDate=dayInt.toString()+" "+monthString+" "+yearString;
-            //check if we want temp in celsius or farenheit-temp in currentConditions in api json is in farenheit        
-            if (incelsius===true){
-                temp=(temp-32)*5/9;
-                feelslike=(feelslike-32)*5/9;
-            }
-            else{
-                tempsymbol='°F'
-            }
+            
+            
             tempval=temp;
             feelslikeval=feelslike;     
             document.getElementById('errormessage').innerHTML='';
